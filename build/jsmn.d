a840d2617de910edb49eff4810d6140dc490739f
KunoichiZ <kotonesoul@yahoo.com>
2017-04-21 15:42:16 -0500

    Add build folder

---
+++ build/jsmn.d
@@ -0,0 +1,5 @@
+jsmn.o: \
+ c:/Users/koton/Documents/GitHub/SoVJP-plugin-downloader/source/jsmn.c \
+ c:/Users/koton/Documents/GitHub/SoVJP-plugin-downloader/source/jsmn.h
+
+c:/Users/koton/Documents/GitHub/SoVJP-plugin-downloader/source/jsmn.h:
